cc9fc86acaa3f30b58483ee9603e2e13036d880c
Jayana Nanayakkara <jchdnjc@gmail.com>
2025-06-25 12:38:04 +0530

    created function to insert image embedding into database

---
+++ db.py
@@ -0,0 +1,43 @@
+import psycopg2
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+
+DB_HOST = os.getenv("DB_HOST", "localhost")
+DB_NAME = os.getenv("DB_NAME", "postgres")
+DB_USER = os.getenv("DB_USER", "postgres")
+DB_PASSWORD = os.getenv("DB_PASSWORD", "")
+
+conn = psycopg2.connect(
+    host=DB_HOST,
+    database=DB_NAME,
+    user=DB_USER,
+    password=DB_PASSWORD,
+    port=5432
+)
+
+def insert_image_embedding(filename, file_path, embedding):
+    embedding = embedding.tolist() 
+    cur = conn.cursor()
+    cur.execute("INSERT INTO image_embeddings (filename, file_path, embedding) VALUES(%s, %s, %s)", (filename, file_path, embedding))
+    conn.commit()
+    cur.close()
+
+
+conn.close()
+
+
+
+
+"""
+Created table for immage embeddings
+
+CREATE TABLE IF NOT EXISTS image_embeddings (
+    id SERIAL PRIMARY KEY,
+    filename VARCHAR(255) NOT NULL,
+    file_path VARCHAR(500) NOT NULL,
+    embedding vector(512),
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+);
+"""
